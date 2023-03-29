ec18ec068b980a1247b915fb83c60649469717f9
Iris <lin.tf1124@gmail.com>
2023-03-30 00:28:45 +0800

    更改skill icon位置

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -33,97 +33,97 @@
                 skill: [
                     {
                         name: 'HTML',
-                        imgUrl: require('@/assets/images/html.png'),
+                        imgUrl: require('@/assets/images/icon/html.png'),
                         type: '程式語言',
                         isMainSkill: true
                     },
                     {
                         name: 'CSS / SCSS',
-                        imgUrl: require('@/assets/images/css.png'),
+                        imgUrl: require('@/assets/images/icon/css.png'),
                         type: '程式語言',
                         isMainSkill: true
                     },
                     {
                         name: 'VUE3',
-                        imgUrl: require('@/assets/images/vue.png'),
+                        imgUrl: require('@/assets/images/icon/vue.png'),
                         type: '程式語言',
                         isMainSkill: true
                     },
                     {
                         name: 'BOOTSTRAP',
-                        imgUrl: require('@/assets/images/bootstrap.png'),
+                        imgUrl: require('@/assets/images/icon/bootstrap.png'),
                         type: '程式語言',
                         isMainSkill: true
                     },
                     {
                         name: 'VB.NET',
-                        imgUrl: require('@/assets/images/vb.png'),
+                        imgUrl: require('@/assets/images/icon/vb.png'),
                         type: '程式語言',
                         isMainSkill: true
                     },
                     {
                         name: 'MMSQL',
-                        imgUrl: require('@/assets/images/sql.png'),
+                        imgUrl: require('@/assets/images/icon/sql.png'),
                         type: '程式語言',
                         isMainSkill: true
                     },
                     {
                         name: 'Visual Studio',
-                        imgUrl: require('@/assets/images/vs.png'),
+                        imgUrl: require('@/assets/images/icon/vs.png'),
                         type: '開發工具',
                         isMainSkill: false
                     },
                     {
                         name: 'Visual Studio Code',
-                        imgUrl: require('@/assets/images/vscode.png'),
+                        imgUrl: require('@/assets/images/icon/vscode.png'),
                         type: '開發工具',
                         isMainSkill: false
                     },
                     {
                         name: 'Sublime',
-                        imgUrl: require('@/assets/images/sublime.png'),
+                        imgUrl: require('@/assets/images/icon/sublime.png'),
                         type: '開發工具',
                         isMainSkill: false
                     },
                     {
                         name: 'GitHub',
-                        imgUrl: require('@/assets/images/git.png'),
+                        imgUrl: require('@/assets/images/icon/git.png'),
                         type: '版本控制工具',
                         isMainSkill: true
                     },
                     {
                         name: 'Adobe XD',
-                        imgUrl: require('@/assets/images/xd.png'),
+                        imgUrl: require('@/assets/images/icon/xd.png'),
                         type: 'UI設計軟體',
                         isMainSkill: true
                     },
                     {
                         name: 'Sketch',
-                        imgUrl: require('@/assets/images/sketch.png'),
+                        imgUrl: require('@/assets/images/icon/sketch.png'),
                         type: 'UI設計軟體',
                         isMainSkill: false
                     },
                     {
                         name: 'Adobe Photoshop',
-                        imgUrl: require('@/assets/images/ps.png'),
+                        imgUrl: require('@/assets/images/icon/ps.png'),
                         type: '設計軟體',
                         isMainSkill: false
                     },
                     {
                         name: 'Adobe Illustrator',
-                        imgUrl: require('@/assets/images/ai.png'),
+                        imgUrl: require('@/assets/images/icon/ai.png'),
                         type: '設計軟體',
                         isMainSkill: false
                     },
                     {
                         name: 'Adobe Lightroom',
-                        imgUrl: require('@/assets/images/lr.png'),
+                        imgUrl: require('@/assets/images/icon/lr.png'),
                         type: '影像軟體',
                         isMainSkill: false
                     },
                     {
                         name: 'Wordpress',
-                        imgUrl: require('@/assets/images/wordpress.png'),
+                        imgUrl: require('@/assets/images/icon/wordpress.png'),
                         type: '模板工具',
                         isMainSkill: false
                     },
