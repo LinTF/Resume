22cc2e12a865e279f668f6671ebc2e1d0e972542
Iris <lin.tf1124@gmail.com>
2023-04-04 01:16:43 +0800

    測試燈箱

---
+++ src/components/LightBox.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">{{ propsWorkInfo.title }}</h5>
+              <p>{{ propsWorkInfo.note }}</p>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div>
+                            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                                <div class="carousel-indicators">
+                                    <button type="button" 
+                                        data-bs-target="#carouselExampleIndicators" 
+                                        v-for="(imgUrl, index) in propsWorkInfo.moreImgUrl" 
+                                        :key="title + index" 
+                                        :data-bs-slide-to="index" 
+                                        class="active" 
+                                        :aria-current="index === 0" 
+                                        :aria-label="'Slide ' + index"></button>
+                                </div>
+                                <div class="carousel-inner">
+                                    <div 
+                                        v-for="(imgUrl, index) in propsWorkInfo.moreImgUrl" 
+                                        :key="title + index"
+                                        :class="['carousel-item', { active: index === 0 }]">
+                                        <img :src="imgUrl" class="d-block w-100">
+                                    </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div>
+                            <p>{{ propsWorkInfo.type }}</p>
+                            <a>{{ propsWorkInfo.linkUrl }}</a>
+                            <p>{{ propsWorkInfo.directions }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">已閱讀</button>
+            </div>
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LightBox',
+        props: {
+            propsWorkInfo: {
+                type: Object,
+                required: true,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Works.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="row">
+        <div class="col-md-4" v-for="(word, index) in works" :key="index">
+            <a class="work-item" data-bs-toggle="modal" data-bs-target="#exampleModal" @click="workInfo(index)">
+                <img :src="word.imgUrl" />
+                <div class="mask vertical-center">
+                    <div>
+                        <h3>{{ word.title }}</h3>
+                        <p>{{ word.note }}</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Works',
+        data() {
+            return {
+                works: [
+                    {
+                        title: '3M全戶淨水系統',
+                        note: '切版與設計',
+                        imgUrl: require('@/assets/images/code/3M/index.png'),
+                        type: [ 'Bootstrap', 'SCSS' ],
+                        directions: '3M全戶淨水系統產品使用場域介紹的網頁設計與切版',
+                        linkUrl: 'https://findmyfilter.com.tw/',
+                        moreImgUrl: [
+                            require('@/assets/images/code/3M/index.png'),
+                            require('@/assets/images/code/incom/index.png'),
+                            require('@/assets/images/code/Moto7/1.png')
+                        ]
+                    },
+                    {
+                        title: '收支分析表',
+                        note: '全前端',
+                        imgUrl: require('@/assets/images/code/incom/index.png'),
+                        type: [ 'vue', 'Bootstrap', 'SCSS' ],
+                        directions: '這是使用 Vue 做的第一個 side project，了解 Vue 的建立、元件使用、props傳入、$emit拋出、components、computed、 methods、打包專案等',
+                        linkUrl: 'https://lintf.github.io/IncomePlan/',
+                        moreImgUrl: [
+                            require('@/assets/images/code/incom/index.png')
+                        ]
+                    }
+                ]
+            }
+        },
+        methods: {
+            workInfo(val) {
+                this.$emit('emitWork', this.works[val])
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .work-item {
+        position: relative;
+
+        img {
+            width: 100%;   
+            height: 100%;   
+            object-fit:cover;
+        }
+
+        .mask {
+            width: 100%;
+            height: 100%;
+            background-color: #000000;
+            position: absolute;
+            opacity: 0;
+            top: 0px;
+
+            h3, p {
+                color: #fff;
+                text-align: center;
+            }
+
+            &>div {
+                width: inherit;
+            }
+        }
+
+        &:hover .mask {
+            opacity: .8;
+        }
+    }
+</style>
