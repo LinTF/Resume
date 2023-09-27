1b5b38f99dfad391ca5d09e60e24c54b2a4983d5
Iris <lin.tf1124@gmail.com>
2023-09-27 18:01:50 +0800

    調整下方聯絡我們區塊的排版與樣式

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,44 +1,31 @@
 <template>
-    <div class="row">
-        <div class="col-md-6 vertical-center">
+    <div>
+        <div class="vertical-center">
             <div id="info">
                 <div class="name-block">
-                    <h3>林婷芳</h3> <span>Iris</span>
+                    <h3>林婷芳</h3>
+                    <p>Iris</p>
                 </div>
                 <hr/>
-                <div class="contact-item">
-                    <div class="icon">
-                        <i class="fa-solid fa-mobile-screen-button"></i>
-                    </div>
-                    <a href="tel:0910290508">0910290508</a>
-                </div>
-                
-                <div class="contact-item">
-                    <div class="icon">
-                        <i class="fa-solid fa-envelope-open-text"></i>
-                    </div>
-                    <a href="mailto:lin.tf1124@gmail.com">lin.tf1124@gmail.com</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div id="message">
-                <div>
-                    <h3>與我聯絡</h3>
-                </div>
-                <form action="mailto:lin.tf1124@gmail.com" method="GET" autocomplete="off">
-                    <div class="row">
-                        <div class="mb-3">
-                            <label for="formEmail" class="form-label">聯絡事項</label>
-                            <input type="text" class="form-control" id="formEmail" placeholder="請簡述聯絡事項，例如：邀請面試，外包網站建置" name="subject">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="contact-item">
+                            <div class="icon">
+                                <i class="fa-solid fa-mobile-screen-button"></i>
+                            </div>
+                            <a href="tel:0910290508">與我通話</a>
                         </div>
-                        <div class="mb-3">
-                            <label for="formMessage" class="form-label">訊息</label>
-                            <textarea class="form-control" id="formMessage" rows="2" name="body" placeholder="記得留下姓名，好讓我稱呼您"></textarea>
-                        </div>
-                        <input type="submit" value="送出" class="btn-main">
                     </div>
-                </form>
+                    
+                    <div class="col-6">
+                        <div class="contact-item">
+                            <div class="icon">
+                                <i class="fa-solid fa-envelope-open-text"></i>
+                            </div>
+                            <a href="mailto:lin.tf1124@gmail.com">聯絡我</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
