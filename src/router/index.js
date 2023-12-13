33696a69b39f5c0cb31d8004f14f7e085518bb81
Iris <lin.tf1124@gmail.com>
2023-12-13 22:40:42 +0800

    完成 github pages 畫面重整 404 問題

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
 import IndexView from '../pages/index/Index.vue'
 import AboutView from '../pages/about/About.vue'
 import PortfolioView from '../pages/portfolio/Portfolio.vue'
-// import ErrorView from '../views/404.vue'
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'home',
-  //   component: HomeView
-  // },
   {
     path: '/',
     name: 'index',
     component: IndexView
   },
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  // }
   {
     path: '/about',
     name: 'about',
@@ -33,13 +18,7 @@ const routes = [
     path: '/portfolio',
     name: 'portfolio',
     component: PortfolioView
-  },
-  // {
-  //   path: '/:catchAll(.*)',
-  //   // path: '*',
-  //   name: '404',
-  //   component: ErrorView
-  // }
+  }
 ]
 
 const router = createRouter({
